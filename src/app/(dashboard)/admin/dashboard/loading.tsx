a455ac7e4fe44f4bdb4180ae29a10c3f9b7ace23
ujjal <ujjalroy7862@gmail.com>
2026-05-07 23:07:33 +0600

    feat(admin): add dashboard loading state

---
+++ src/app/(dashboard)/admin/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+import { SkAdminDashboard, SkDashboardHeader } from "@/components/common/skeletons"
+export default function Loading() {
+  return (
+    <div>
+      <SkDashboardHeader />
+      <SkAdminDashboard />
+    </div>
+  )
+}
