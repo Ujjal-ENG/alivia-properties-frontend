1fe98ca977de65b7411ebe083c6be6a4525d48ed
Ujjal Roy <ujjalroy7862@gmail.com>
2026-05-29 03:03:55 +0600

    chore(mock-api): remove properties/store.ts

--- src/app/api/mock/properties/store.ts
+++
@@ -1,4 +0,0 @@
-import { DUMMY_PROPERTIES } from "@/data/dummy-properties"
-import type { Property } from "@/types/property.types"
-
-export const propertiesStore: Property[] = [...DUMMY_PROPERTIES]
